3f5ed090fd405a9e23adc651422523790ecac4a9
SATRIA HATIM MARRENTO <nito.43325221@mhs.polines.ac.id>
2026-04-28 10:09:48 +0700

    Delete cungpruy.py

--- cungpruy.py
+++
@@ -1,9 +0,0 @@
-class Person:
-    def ___init__ (self, nama, umur):
-        self.nama = nama
-        self.umur = umur
-    def tampilkan_info(self):
-        print("Nama: ",self.nama)
-        print("Umur: ",self.umur)
-
-    
